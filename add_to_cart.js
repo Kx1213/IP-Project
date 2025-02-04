088143ebcab22641ba10d7f10b05ed72021fdc63
Kx1213 <s10267360@connect.np.edu.sg>
2025-02-04 10:06:29 +0800

    hehe

--- add_to_cart.js
+++ add_to_cart.js
@@ -5,6 +5,7 @@ fetch('https://6796ea9ebedc5d43a6c6565f.mockapi.io/api/v1/product')
     const urlParams = new URLSearchParams(window.location.search);
     const productId = urlParams.get('id'); // Extract the ID from the URL query parameterß
 
+    console.log('test');
     // Find the product matching the ID
     const product = data.find(p => p.id === productId);
     if (product) {
@@ -21,16 +22,25 @@ fetch('https://6796ea9ebedc5d43a6c6565f.mockapi.io/api/v1/product')
                 headers: {
                     'Content-Type': 'application/json'
                 },
+                /*
                 body: JSON.stringify({ 
                     productId: productId,
                     title: product.title,
                     price: product.price,
                     image: product.image,
                     quantity: 1
+                })*/
+                body: JSON.stringify({ 
+                    productId: 5,
+                    title: 't',
+                    price: 111,
+                    image: 'ttt',
+                    quantity: 1
                 })
             })
             .then(response => response.json())
             .then(data => {
+                console.log('test');
                 alert('Product added to cart!');
             })
             .catch(err => console.error('Error adding product to cart:', err));
@@ -42,6 +52,36 @@ fetch('https://6796ea9ebedc5d43a6c6565f.mockapi.io/api/v1/product')
     }
 })
 .catch(err => console.error('Error fetching product data:', err));
+
+document.querySelector('.add-to-cart').addEventListener('click', function() {
+    fetch('https://6796ea9ebedc5d43a6c6565f.mockapi.io/api/v1/cart', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        
+        body: JSON.stringify({ 
+            productId: productId,
+            title: product.title,
+            price: product.price,
+            image: product.image,
+            quantity: 1
+        })
+        /*
+        body: JSON.stringify({ 
+            productId: 5,
+            title: 't',
+            price: 111,
+            image: 'ttt',
+            quantity: 1
+        })*/
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('test');
+        alert('Product added to cart!');
+    })
+});
 
 // Function to go back to the shop
 function goBackToShop() {
